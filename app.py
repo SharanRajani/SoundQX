9d230d5a67e2aafe3c71f3506368795887c162b8
saksham <sakshambassi@gmail.com>
2019-03-17 12:29:02 +0530

    selected file in static

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request
+from werkzeug import secure_filename
+
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
 	if request.method == 'POST':
 		f = request.files['file']
-		# f.save(secure_filename(f.filename))
+		f.save('./static/wav/'+secure_filename(f.filename))
 		print(f)
 	return render_template('one.html')
 
