00d4a534f43985770a2531e0a8ef0007a6d8b1fd
saksham <sakshambassi@gmail.com>
2019-03-17 16:44:01 +0530

    Post request solved

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
+	f = 'wav/no_tree_ent.wav'
 	if request.method == 'POST':
 		f = request.files['file']
+		print(f.filename)
 		f.save('./static/wav/'+secure_filename(f.filename))
-		print(f)
-	return render_template('one.html')
+		f = 'wav/'+f.filename
+	return render_template('one.html', wav_file = f)
 
 @app.route('/flash_spec')
 def display_spec():
