5c032f9a69f07d98ef3d580c8231146b3d6cdc33
thegodfather101 <thesharanrajani@gmail.com>
2019-05-29 17:57:47 +0530

    New Second page half done. #3

--- createAmpGraph.py
+++ createAmpGraph.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.set(font_scale = 1.8)
+sns.set(font_scale = 0.9)
 
 
 
 def makegraph(y):
-	plt.figure(figsize=(20,10))
+	plt.figure(figsize=(9,4))
 	# plt.xkcd()
 	plt.xlabel("Time Stamps(44.1k/sec)")
 	plt.ylabel("Amplitude")
 	plt.plot(y, c = "#548194")
-	plt.savefig("./static/images/AmpVsTime.png")
+	plt.savefig("./static/images/AmpVsTime.png", bbox_inches = 'tight')
